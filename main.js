2fff147020c65e5d19f280bfdf389a8b4810eb84
Paras <parasfahim26@gmail.com>
2023-05-08 23:22:08 +0500

    -a

--- main.js
+++ main.js
@@ -11,44 +11,7 @@ window.onscroll = () =>{
     navbar.classList.remove('active');
 };
 
-var swiper = new Swiper(".home-slider", {
-    loop:true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
  
-  var swiper = new Swiper(".card-slider", {
-    spaceBetween: 30,
-    loop:true,
-    speed: 1000,
-    // autoplay:{
-    //   delay: 4000
-    // },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-      },
-      480: {
-        slidesPerView: 2,
-      },
-      768: {
-        slidesPerView: 3,
-      },
-      1200: {
-        slidesPerView: 4,
-      },
-    },
-  });  
   var swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
